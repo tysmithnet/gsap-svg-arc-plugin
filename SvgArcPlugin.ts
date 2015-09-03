e577800c219b8d065dff4f7903670b3d8a2d5017
ty <ty@tysmith.net>
2015-09-02 21:11:46 -0400

    Added grunt and split plugin.js

---
+++ SvgArcPlugin.ts
@@ -0,0 +1,55 @@
+///<reference path="snapsvg.d.ts"/>
+
+interface IPluginOptions {
+    x:number;
+    y:number;
+    startAngle: number;
+    arcDegrees: number;
+    thickness: number;
+}
+
+function isNumeric(value:any):boolean {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+}
+
+class SvgArcPlugin {
+    propName:string = "typed";
+    API:number = 2;
+    version:string = "1.0.2";
+    target:ISvgArc;
+    options:IPluginOptions;
+
+    init(target:ISvgArc, value:IPluginOptions, tween:any):boolean {
+        if (!this.validate(target))
+            return false;
+
+        this.prepareTarget(target);
+        this.target = target;
+        this.options = value;
+    }
+
+    validate(target:any):boolean {
+        if (target.container == null || !(target instanceof SVGElement)) {
+            console.log("target.container must be set to an SVGElement");
+            return false;
+        }
+    }
+
+    prepareTarget(target:any) {
+        target.x = target.x || 0;
+        target.y = target.y || 0;
+        target.startAngle = target.startAngle || 0;
+        target.arcDegrees = target.arcDegress || 0;
+        target.thickness = target.thickness || 1;
+    }
+
+    set(ratio:number):void {
+        var x = isNumeric(this.options.x) ? this.scaleValue(this.target.x, this.options.x, ratio) : this.target.x;
+        var y = isNumeric(this.options.y) ? this.scaleValue(this.target.y, this.options.y, ratio) : this.target.y;
+        var y = isNumeric(this.options.y) ? this.scaleValue(this.target.y, this.options.y, ratio) : this.target.y;
+    }
+
+    scaleValue(start:number, end:number, ratio:number):number {
+        return start + ((end - start) * ratio);
+    }
+}
--- SvgArg.ts
+++ SvgArg.ts
@@ -1,13 +1,5 @@
 ///<reference path="snapsvg.d.ts"/>
 
-interface IPluginOptions {
-    x:number;
-    y:number;
-    startAngle: number;
-    arcDegrees: number;
-    thickness: number;
-}
-
 class SvgArc {
     private snap:Snap;
     private arc:Snap.Element;
@@ -55,50 +47,4 @@ class SvgArc {
         return d;
     }
 
-}
-
-function isNumeric(value:any):boolean {
-    return !isNaN(parseFloat(value)) && isFinite(value);
-}
-
-class SvgArcPlugin {
-    propName:string = "typed";
-    API:number = 2;
-    version:string = "1.0.2";
-    target:ISvgArc;
-    options:IPluginOptions;
-
-    init(target:ISvgArc, value:IPluginOptions, tween:any):boolean {
-        if (!this.validate(target))
-            return false;
-
-        this.prepareTarget(target);
-        this.target = target;
-        this.options = value;
-    }
-
-    validate(target:any):boolean {
-        if (target.container == null || !(target instanceof SVGElement)) {
-            console.log("target.container must be set to an SVGElement");
-            return false;
-        }
-    }
-
-    prepareTarget(target:any) {
-        target.x = target.x || 0;
-        target.y = target.y || 0;
-        target.startAngle = target.startAngle || 0;
-        target.arcDegrees = target.arcDegress || 0;
-        target.thickness = target.thickness || 1;
-    }
-
-    set(ratio:number):void {
-        var x = isNumeric(this.options.x) ? this.scaleValue(this.target.x, this.options.x, ratio) : this.target.x;
-        var y = isNumeric(this.options.y) ? this.scaleValue(this.target.y, this.options.y, ratio) : this.target.y;
-        var y = isNumeric(this.options.y) ? this.scaleValue(this.target.y, this.options.y, ratio) : this.target.y;
-    }
-
-    scaleValue(start:number, end:number, ratio:number):number {
-        return start + ((end - start) * ratio);
-    }
 }
